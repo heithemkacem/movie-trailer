efb47b98d2524139e5eb44bc6c056b3f39876093
Heithem Kacem <heithemkacem2016@gmail.com>
2022-05-30 00:29:52 +0100

    git  commit

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '614f0dc948da7e452319587c96b9ad03',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
